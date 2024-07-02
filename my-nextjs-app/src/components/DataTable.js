c82717c2c08fa19f325177282ca48c76e9a1aa0e
lordcowie <mootezjebalia@gmail.com>
2024-07-02 08:13:37 -0700

    displaying the datatable

--- my-nextjs-app/src/components/DataTable.js
+++ my-nextjs-app/src/components/DataTable.js
@@ -1,3 +1,4 @@
+'use client'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
@@ -6,11 +7,44 @@ const DataTable = () => {
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response=> setData(response.data))
-        .catch(error => console.error('error fetching data:', error));
+            .then(response => setData(response.data))
+            .catch(error => console.error('error fetching data:', error));
     }, []);
 
+    return (
+        <div>
+            <h1> Posts </h1>
+            <table border="1" style={{ width: '100%', textAlign: 'left' }} >
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(post => ( 
+                        <tr key = {post.id}>
+                           <td>{post.id}</td> 
+                           <td>{post.title}</td> 
+                           <td>{post.body}</td> 
+                        </tr>
+                    ))}
 
+
+                </tbody>
+
+
+
+            </table>
+
+
+
+
+        </div>
+
+
+    );
 
 
 };
