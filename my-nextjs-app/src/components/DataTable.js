26b57e51c0e62e7a8ecd8960680c0596c3368d4c
lordcowie <mootezjebalia@gmail.com>
2024-07-02 08:57:25 -0700

    implemented sorting feature for table headers

--- my-nextjs-app/src/components/DataTable.js
+++ my-nextjs-app/src/components/DataTable.js
@@ -8,6 +8,8 @@ const DataTable = () => {
     const [search, setSearch] = useState('');
     const [currentPage, setCurrentPage] = useState(1);
     const [postsPerPage] = useState(10); // number of posts per page
+    const [sortConfig, setSortConfig] = useState({key : '', direction:''});
+
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -27,16 +29,29 @@ const DataTable = () => {
         )
         setFilteredData(filtered);
         setCurrentPage(1); // reset to first page on new search 
+    }
+    
+    //sorting table headers
+    const handleSort = (key) => {
+      let direction = 'ascending';
+      if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+        direction = 'descending';
+      }
+      setSortConfig({ key, direction });
 
+      const sortedData = [...filteredData].sort((a,b) => {
+        if (a[key] < b[key]) {
+            return direction === 'ascending' ? -1 : 1;
+        }
+        if (a[key] > b[key]) {
+            return direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+
+      });
+      setFilteredData(sortedData);
 
     }
-
-
-
-
-
-
-
 
 
     //slice the data for current page 
@@ -61,9 +76,9 @@ const DataTable = () => {
             <table border="1" style={{ width: '100%', textAlign: 'left' }} >
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Title</th>
-                        <th>Body</th>
+                        <th onClick={() => handleSort('id')}>ID</th>
+                        <th onClick={() => handleSort('title')}>Title</th>
+                        <th onClick={() => handleSort('body')}>Body</th>
                     </tr>
                 </thead>
                 <tbody>
