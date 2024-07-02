12a2133e62611a16d39395e3ac8264cc1d895ee5
lordcowie <mootezjebalia@gmail.com>
2024-07-02 08:25:22 -0700

    implementing pagination

--- my-nextjs-app/src/components/DataTable.js
+++ my-nextjs-app/src/components/DataTable.js
@@ -4,12 +4,29 @@ import { useEffect, useState } from 'react';
 
 const DataTable = () => {
     const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(10); // number of posts per page
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => setData(response.data))
             .catch(error => console.error('error fetching data:', error));
     }, []);
+
+    //slice the data for current page 
+    const indexofLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexofLastPost - postsPerPage;
+    const currentPosts = data.slice(indexOfFirstPost, indexofLastPost);
+
+    //change page 
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    // total number of pages 
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(data.length / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
 
     return (
         <div>
@@ -23,11 +40,11 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map(post => ( 
-                        <tr key = {post.id}>
-                           <td>{post.id}</td> 
-                           <td>{post.title}</td> 
-                           <td>{post.body}</td> 
+                    {currentPosts.map(post => (
+                        <tr key={post.id}>
+                            <td>{post.id}</td>
+                            <td>{post.title}</td>
+                            <td>{post.body}</td>
                         </tr>
                     ))}
 
@@ -37,9 +54,13 @@ const DataTable = () => {
 
 
             </table>
-
-
-
+         <div>
+            {pageNumbers.map(number => (
+                <button key = {number} onClick={() => paginate(number)}>
+                 {number}
+                </button>
+            ))}
+         </div>
 
         </div>
 
